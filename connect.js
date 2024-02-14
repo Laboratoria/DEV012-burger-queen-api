c9409d6a9567be2ea17a70ede4d029df59be138c
barbara <benboxuwu@gmail.com>
2024-02-13 23:47:36 -0600

    connect

--- connect.js
+++ connect.js
@@ -1,10 +1,23 @@
+const { MongoClient } = require('mongodb');
 const config = require('./config');
 
 // eslint-disable-next-line no-unused-vars
 const { dbUrl } = config;
 
+const client = new MongoClient(dbUrl);
+
 async function connect() {
-  // TODO: Database Connection
+  try {
+    // Conectar al cliente de MongoDB
+    await client.connect();
+    console.log('Conectado a MongoDB');
+
+    const db = client.db('burger_queen');
+    console.log('Conexión verificada');
+    return db;
+  } catch (error) {
+    console.error('Error al conectar a MongoDB:', error);
+  }
 }
 
 module.exports = { connect };
